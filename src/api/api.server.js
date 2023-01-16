0403dc98607959dd88dbd0d6172b8131b37ea03a
Esteban <esteban16.rodas@gmail.com>
2023-01-16 10:39:49 -0500

    Updating deploy url

--- src/api/api.server.js
+++ src/api/api.server.js
@@ -1,3 +1,2 @@
-const API_SERVER = "https://filmaniatics-api.herokuapp.com";
-//const API_SERVER = "http://localhost:5000"; // PARA DEV-TEST
+const API_SERVER = "https://filmaniatics-backend.onrender.com";
 export default API_SERVER;
